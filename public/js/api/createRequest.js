f7453b486433424f427c1d419671e312549970f0
SVETA <svetadan1234@mail.ru>
2026-04-24 16:07:43 +0500

    first commit createRequest

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,25 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
-const createRequest = (options = {}) => {
+function createRequest(options) {
+    const xhr = new XMLHttpRequest();
 
-};
+    const url = options.url;
+    const data = options.data;
+    const method = options.method;
+    const callback = options.callback;
+
+    if (method === "GET" && Object.keys(data).length > 0) {
+    } else {
+
+    }
+    
+    xhr.open(method, url);
+    xhr.responseType = "json";
+
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            callback(xhr.response);
+        } else {
+            const error = new Error(`HTTP error! status: &{xhr.status}`);
+            callback(error);
+        }
+    }
+}
